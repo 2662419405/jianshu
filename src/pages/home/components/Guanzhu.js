0156dbec7abdfd95675ad9219445db9bafaedb83
Qiuhang817385 <826266129@qq.com>
2019-12-27 01:08:50 +0800

    Home-Guanzhu关注页面读取数据完成

--- src/pages/home/components/Guanzhu.js
+++ src/pages/home/components/Guanzhu.js
@@ -1,9 +1,18 @@
 import React, { PureComponent, Fragment } from "react";
 import { connect } from "react-redux";
-import { GuanzjiWarpper, GuanzhuTitle ,GuanzhuFotter,GuanzhuUl} from "../styled";
+import ReactDom from 'react-dom';
+import {
+  GuanzjiWarpper,
+  GuanzhuTitle,
+  GuanzhuFotter,
+  GuanzhuUl,
+  GuanzhuItem,
+  GuanzhuChaoji
+} from "../styled";
 import { Icon } from "antd";
 class Guanzhu extends PureComponent {
   render() {
+    const { List,handleUpdateData } = this.props;
     return (
       <Fragment>
         {/* 盒子容器 */}
@@ -12,7 +21,7 @@ class Guanzhu extends PureComponent {
           <GuanzhuTitle>
             <span>推荐作者</span>
             {/* 换一批 */}
-            <a className="page_change" href="/">
+            <a  onClick={handleUpdateData} className="page_change" href="/">
               <Icon
                 id="iconPic"
                 type="redo"
@@ -28,7 +37,25 @@ class Guanzhu extends PureComponent {
               换一批 >
             </a>
           </GuanzhuTitle>
-          <GuanzhuUl></GuanzhuUl>
+          <GuanzhuUl>
+            {List.map(item => {
+              return (
+                <GuanzhuItem key={item.get("id")}>
+                  <a className="avatar" href="/">
+                    <img alt="" src={item.get("imgUrl")} />
+                  </a>
+                  <GuanzhuChaoji>
+                    <Icon type="plus" />
+                    关注
+                  </GuanzhuChaoji>
+                  <a href="/" className="name">
+                    {item.get("userName")}
+                  </a>
+                  <p>{item.get("content")}</p>
+                </GuanzhuItem>
+              );
+            })}
+          </GuanzhuUl>
           {/* 查看全部 */}
           <GuanzhuFotter>查看全部 ></GuanzhuFotter>
         </GuanzjiWarpper>
@@ -37,4 +64,25 @@ class Guanzhu extends PureComponent {
   }
 }
 
-export default connect(null, null)(Guanzhu);
+const mapStatetoProp = state => {
+  return {
+    List: state.get("home").get("GuanzhuList")
+  };
+};
+
+
+const mapDispatchToProps = (dispatch)=>({
+    // 换一换动画
+    handleUpdateData(e){
+        e.preventDefault();
+        const aHref = ReactDom.findDOMNode(document.getElementById('iconPic'));
+        let originAngle = aHref.style.transform.replace(/[^0-9]/ig,'');
+        if(originAngle){
+            originAngle=parseInt(originAngle,10);
+        }else{
+            originAngle=0;
+        }
+        aHref.style.transform = 'rotate(' + (originAngle+360) + 'deg)';
+    }   
+})
+export default connect(mapStatetoProp, mapDispatchToProps)(Guanzhu);
